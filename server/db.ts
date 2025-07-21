c133b131bf7a46774dfb562b55fadabda6007ee4
johnreesekenya2 <johnreeserwanda@gmail.com>
2025-07-22 00:21:53 +0300

    Add files via upload

--- server/db.ts
+++ server/db.ts
@@ -1,13 +1,11 @@
 import { drizzle } from 'drizzle-orm/postgres-js';
-import { Pool } from 'pg';
+import postgres from 'postgres';
 import * as schema from '@shared/schema';
 
 if (!process.env.DATABASE_URL) {
   throw new Error('DATABASE_URL must be set');
 }
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-});
+const sql = postgres(process.env.DATABASE_URL);
 
-export const db = drizzle(pool, { schema });
+export const db = drizzle(sql, { schema });
